7df1858edf2d0834bd3278e946cb94e6587b738a
gemaquejr <gemaquejr@hotmail.com>
2023-03-26 21:18:19 -0300

    Adiciona IMotorcycle.ts

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+import { vehicleZodSchema } from './IVehicle';
+
+const motorcycleZodSchema = vehicleZodSchema.extend({
+category: z.enum(['Street', 'Custom', 'Trail']),
+engineCapacity: z.number().min(1).max(2500) });
+
+type IMotorcycle = z.infer<typeof motorcycleZodSchema>;
+
+export { motorcycleZodSchema, IMotorcycle };
