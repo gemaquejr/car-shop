7f76be0acac8330d5fd97aba2ebb56b36b44d2a4
gemaquejr <gemaquejr@hotmail.com>
2023-03-26 21:38:26 -0300

    Adiciona refatoracao de services.car.test.ts

--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -1,11 +1,13 @@
 import * as sinon from "sinon";
 import chai from "chai";
 const { expect } = chai;
+import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog'
 
 import CarCreateModel from "../../../models/CarCreateModel";
 import CarCreateService from "../../../services/CarCreateService";
 
-import { carMock } from "../../mocks/carMock";
+import { carMock, carMockWithId } from "../../mocks/carMock";
 
 describe("Car Service", () => {
   const carCreateModel = new CarCreateModel();
@@ -13,6 +15,19 @@ describe("Car Service", () => {
 
   before(async () => {
     sinon.stub(carCreateModel, "create").resolves(carMock);
+    sinon.stub(carCreateModel, 'readOne')
+      .onCall(0).resolves(carMockWithId)
+      .onCall(1).resolves(null);
+
+    sinon.stub(carCreateModel, 'read').resolves([carMockWithId]);
+
+    sinon.stub(carCreateModel, 'update')
+      .onCall(0).resolves(carMockWithId)
+      .onCall(1).resolves(null);
+
+    sinon.stub(carCreateModel, 'delete')
+      .onCall(0).resolves(carMockWithId)
+      .onCall(1).resolves(null);
   });
 
   after(() => {
@@ -23,6 +38,54 @@ describe("Car Service", () => {
     it("successfully created", async () => {
       const newCar = await carCreateService.create(carMock);
       expect(newCar).to.be.deep.equal(carMock);
+    });
+
+    it('Failure', async () => {
+      try {
+        await carCreateService.create({} as any);
+      } catch (error) {
+        expect(error).to.be.instanceOf(ZodError);
+      }
+    });
+  });
+
+  describe('ReadOne car', () => {
+    it('Success', async () => {
+      const carCreated = await carCreateService.readOne(carMockWithId._id);
+
+      expect(carCreated).to.be.deep.equal(carMockWithId);
+    });
+
+    it('Failure', async () => {
+      try {
+        await carCreateService.readOne(carMockWithId._id);
+      } catch (error: any) {
+        expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+      }
+    });
+  });
+
+  describe('Read all cars', () => {
+    it('Success', async () => {
+      const carCreated = await carCreateService.read();
+
+      expect(carCreated).to.be.deep.equal([carMockWithId]);
+    });
+  });
+
+  describe('Deleting a car', () => {
+    it('Success', async () => {
+      const carCreated = await carCreateService.delete(carMockWithId._id);
+
+      expect(carCreated).to.be.deep.equal(carMockWithId);
+    });
+
+    it('Failure', async () => {
+      try {
+        await carCreateService.delete(carMockWithId._id);
+      } catch (error: any) {
+        expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+      }
     });
   });
 });
