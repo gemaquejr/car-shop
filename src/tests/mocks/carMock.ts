1db8ca91b0fa832549bf2b04999565fc8737770c
gemaquejr <gemaquejr@hotmail.com>
2023-03-26 21:32:20 -0300

    Adiciona refatoracao de carMock.ts

--- src/tests/mocks/carMock.ts
+++ src/tests/mocks/carMock.ts
@@ -9,4 +9,33 @@ const carMock: ICar = {
     doorsQty: 4,
 }
 
-export { carMock };
+const carMockWithId: ICar & { _id: string } = {
+    _id: "4edd40c86762e0fb12000003",
+    model: 'Peugeot',
+    year: 2022,
+    color: 'gray',
+    buyValue: 110000,
+    seatsQty: 5,
+    doorsQty: 4,
+  }
+  
+const carUpdateMock: ICar = {
+    model: 'Peugeot',
+    year: 2022,
+    color: 'gray',
+    buyValue: 150000,
+    seatsQty: 5,
+    doorsQty: 4,
+  }
+  
+const carUpdateMockWithId: ICar & { _id: string } = {
+    _id: "4edd40c86762e0fb12000003",
+    model: 'Peugeot',
+    year: 2022,
+    color: 'gray',
+    buyValue: 150000,
+    seatsQty: 5,
+    doorsQty: 4,
+  }
+
+export { carMock, carMockWithId, carUpdateMock, carUpdateMockWithId };
