87fe4c8ea3b298109889c06a1bb25c62b2798235
gemaquejr <gemaquejr@hotmail.com>
2023-03-26 21:36:49 -0300

    Adiciona refatoracao de models.car.test.ts

--- src/tests/unit/models/car.test.ts
+++ src/tests/unit/models/car.test.ts
@@ -3,7 +3,7 @@ import chai from "chai";
 const { expect } = chai;
 
 import CarCreateModel from "../../../models/CarCreateModel";
-import { carMock } from "../../mocks/carMock";
+import { carMock, carMockWithId, carUpdateMock, carUpdateMockWithId } from "../../mocks/carMock";
 import { Model } from "mongoose";
 
 describe("Car Model", () => {
@@ -11,6 +11,10 @@ describe("Car Model", () => {
 
   before(async () => {
     sinon.stub(Model, "create").resolves(carMock);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
+    sinon.stub(Model, 'find').resolves([carMockWithId]);
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(carUpdateMockWithId);
+    sinon.stub(Model, 'findByIdAndRemove').resolves(carMockWithId);
   });
 
   after(() => {
@@ -21,6 +25,58 @@ describe("Car Model", () => {
     it("successfully created", async () => {
       const newCar = await carCreateModel.create(carMock);
       expect(newCar).to.be.deep.equal(carMock);
+    });
+  });
+
+  describe('searching a car', () => {
+    it('successfully found', async () => {
+      const CarsFound = await carCreateModel.readOne('62cf1fc6498565d94eba52cd');
+      expect(CarsFound).to.be.deep.equal(carMockWithId);
+    });
+
+    it('_id invalid', async () => {
+      try {
+        await carCreateModel.readOne('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
+
+  describe('searching all cars', () => {
+    it('returns all Cars in the db', async () => {
+      const CarsFound = await carCreateModel.read();
+      expect(CarsFound).to.be.deep.equal([carMockWithId]);
+    });
+  });
+
+  describe('updating a car', () => {
+    it('successfully deleted', async () => {
+      const CarDeleted = await carCreateModel.update('62cf1fc6498565d94eba52cd', carUpdateMock);
+      expect(CarDeleted).to.be.deep.equal(carUpdateMockWithId);
+    });
+
+    it('_id invalid', async () => {
+      try {
+        await carCreateModel.update('123ERRADO', carUpdateMock);
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
+    });
+  });
+
+  describe('deleting a car', () => {
+    it('successfully deleted', async () => {
+      const CarDeleted = await carCreateModel.delete('62cf1fc6498565d94eba52cd');
+      expect(CarDeleted).to.be.deep.equal(carMockWithId);
+    });
+
+    it('_id invalid', async () => {
+      try {
+        await carCreateModel.delete('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('InvalidMongoId');
+      }
     });
   });
 });
